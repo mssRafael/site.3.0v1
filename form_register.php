5c136fac8f2a180f9cfe20a7160a457890a3fdae
mssRafael <msrafaelshaka64@gmail.com>
2022-01-20 17:29:39 -0300

    form input finalizado

--- form_register.php
+++ form_register.php
@@ -1,1 +1,38 @@
 <?php include "includes/navbar_index.php" ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles_form_register.css">
+    <title>TechShop - Registrar</title>
+</head>
+<body>
+    <section class="main-page-register">
+        <div class="container-input-register">
+            <form method="post" action="cadastrarusuario.php">
+                <div class="input-name">
+                    <div class="icon-name">
+                        <i class="far fa-user"></i>
+                    </div>
+                    <div class="input-typename">
+                        <input type="text" name="name" placeholder="Nome...">
+                    </div>
+                </div>
+                <div class="input-password">
+                    <div class="icon-password">
+                        <i class="fas fa-unlock-alt"></i>
+                    </div>
+                    <div class="input-typepassword">
+                        <input type="password" name="password" placeholder="Senha...">
+                    </div>
+                </div>
+                <div class="input-submit">
+                    <input type="submit" value="Registrar">
+                </div>
+            </form>
+        </div>
+    </section>
+</body>
+</html>
